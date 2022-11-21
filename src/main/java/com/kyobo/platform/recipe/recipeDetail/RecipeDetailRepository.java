f46fc3630eba895672e5d6c38ff95a3808a4ec30
rhtjdwls84 <sungjinko1984@gmail.com>
2022-11-21 09:20:06 +0900

    recipe detail, list 패키지 삭제

--- src/main/java/com/kyobo/platform/recipe/recipeDetail/RecipeDetailRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kyobo.platform.recipe.recipeDetail;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeDetailRepository extends JpaRepository<RecipeDetail, String> {
-	
-}
--- src/main/java/com/kyobo/platform/recipe/recipeList/RecipeListRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kyobo.platform.recipe.recipeList;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecipeListRepository extends JpaRepository<RecipeList, String> {
-	
-}
